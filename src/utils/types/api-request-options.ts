4b5b490bd7f5bb9983d39776ca3bdf4ccf5417f4
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 22:29:36 +0300

    Added `sentences` and `words` query params for specifying the number of generated words and sentences

--- src/utils/types/api-request-options.ts
+++ src/utils/types/api-request-options.ts
@@ -4,4 +4,6 @@ import { ResponseFormat } from './response-format';
 export type ApiRequestOptions = OwoifyProbabilities & {
   amount?: number;
   format?: ResponseFormat;
+  sentences?: number | string;
+  words?: number | string;
 };
