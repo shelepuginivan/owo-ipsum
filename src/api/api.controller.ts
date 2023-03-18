f391ab1de218625b2a39e373483811da454b15f3
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 20:15:09 +0300

    Added query parameter `responseType` and generated content formatting

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -2,29 +2,60 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ApiService } from './api.service';
 import { ApiRequestOptions } from '../utils/types/api-request-options';
 import { OwoifyProbabilities } from '../utils/types/owoify-probabilities';
+import { ResponseFormatService } from '../response-format/response-format.service';
 
 @Controller('api')
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly responseFormatService: ResponseFormatService,
+  ) {}
 
   @Get('words')
-  generateWords(@Query() { amount }: ApiRequestOptions): string[] {
-    return this.apiService.generateWords(amount);
+  generateWords(
+    @Query() { amount, responseFormat }: ApiRequestOptions,
+  ): string[] | string {
+    const generatedWords = this.apiService.generateWords(amount);
+
+    return this.responseFormatService.formatResponse(
+      generatedWords,
+      responseFormat,
+    );
   }
 
   @Get('sentences')
   generateSentences(
-    @Query() { amount, action, face, stutter }: ApiRequestOptions,
-  ): string[] {
+    @Query()
+    { amount, action, face, stutter, responseFormat }: ApiRequestOptions,
+  ): string[] | string {
     const probabilities: OwoifyProbabilities = { action, face, stutter };
-    return this.apiService.generateSentences(amount, probabilities);
+
+    const generatedSentences = this.apiService.generateSentences(
+      amount,
+      probabilities,
+    );
+
+    return this.responseFormatService.formatResponse(
+      generatedSentences,
+      responseFormat,
+    );
   }
 
   @Get('paragraphs')
   generateParagraphs(
-    @Query() { amount, action, face, stutter }: ApiRequestOptions,
-  ): string[] {
+    @Query()
+    { amount, action, face, stutter, responseFormat }: ApiRequestOptions,
+  ): string[] | string {
     const probabilities: OwoifyProbabilities = { action, face, stutter };
-    return this.apiService.generateParagraphs(amount, probabilities);
+
+    const generatedParagraphs = this.apiService.generateParagraphs(
+      amount,
+      probabilities,
+    );
+
+    return this.responseFormatService.formatResponse(
+      generatedParagraphs,
+      responseFormat,
+    );
   }
 }
