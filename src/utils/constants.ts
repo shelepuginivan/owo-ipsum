6f42979d50799162b747ad3d79875a54381c621b
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 16:27:19 +0300

    Added constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_ACTION_PROBABILITY = 0.05;
+export const DEFAULT_FACE_PROBABILITY = 0.05;
+export const DEFAULT_STUTTER_PROBABILITY = 0.1;
+
+export const DEFAULT_LOREM: string[] = [];
