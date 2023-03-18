446dddab5ccce3706e73b7e62f9c7afb333e5be7
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 19:21:09 +0300

    Implemented `generateWords` and `generateSentences` methods

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiService } from './api.service';
+import { ApiQueryOptions } from '../utils/types/ApiQueryOptions';
+import { OwoifyProbabilities } from '../utils/types/OwoifyProbabilities';
 
 @Controller('api')
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
   @Get('words')
-  generateWords(@Body() { amount, options }): string[] {
-    return this.apiService.generateWords(amount, options);
+  generateWords(@Query() { amount }: ApiQueryOptions): string[] {
+    return this.apiService.generateWords(amount);
+  }
+
+  @Get('sentences')
+  generateSentences(
+    @Query() { amount, action, face, stutter }: ApiQueryOptions,
+  ): string[] {
+    const probabilities: OwoifyProbabilities = { action, face, stutter };
+    return this.apiService.generateSentences(amount, probabilities);
   }
 }
