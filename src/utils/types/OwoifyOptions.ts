4f89785197982c2ab750802274e133e98256c759
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 16:26:20 +0300

    Added type `OwoifyOptions`

---
+++ src/utils/types/OwoifyOptions.ts
@@ -0,0 +1,10 @@
+export type OwoifyOptions = {
+  owoifyMap: Record<string, string>;
+  actionList: string[];
+  faceList: string[];
+  probabilities: {
+    stutter?: number;
+    action?: number;
+    face?: number;
+  };
+};
