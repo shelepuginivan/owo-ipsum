05c4e87baf367dc11f9fd9bb901f03fda08f16f0
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 20:14:19 +0300

    Included ResponseFormatService to providers

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,10 +3,11 @@ import { ApiService } from './api.service';
 import { ApiController } from './api.controller';
 import { LoremService } from '../lorem/lorem.service';
 import { OwoifyService } from '../owoify/owoify.service';
+import { ResponseFormatService } from '../response-format/response-format.service';
 
 @Module({
   imports: [],
   controllers: [ApiController],
-  providers: [ApiService, LoremService, OwoifyService],
+  providers: [ApiService, LoremService, OwoifyService, ResponseFormatService],
 })
 export class ApiModule {}
