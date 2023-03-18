f0702dadf254c2ef6feaf428c80bbdc14bd242ee
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 18:49:40 +0300

    Included services in providers

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiService } from './api.service';
 import { ApiController } from './api.controller';
+import { LoremService } from '../lorem/lorem.service';
+import { OwoifyService } from '../owoify/owoify.service';
 
 @Module({
   imports: [],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [ApiService, LoremService, OwoifyService],
 })
 export class ApiModule {}
