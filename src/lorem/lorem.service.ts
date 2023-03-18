e1be0278ff782df4b60b5a37396b8a43fae2a289
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-18 22:28:46 +0300

    Added additional arguments for specifying amount of words in sentence (wordsAmountRange) and amount of sentences in paragraph (sentenceAmountRange)

--- src/lorem/lorem.service.ts
+++ src/lorem/lorem.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { randomItem } from '../utils/random-item';
-import { DEFAULT_LOREM, LOREM_FIRST } from '../utils/constants';
+import {
+  DEFAULT_LOREM,
+  DEFAULT_MAX_SENTENCES,
+  DEFAULT_MAX_WORDS,
+  DEFAULT_MIN_SENTENCES,
+  DEFAULT_MIN_WORDS,
+  LOREM_FIRST,
+} from '../utils/constants';
 import { randomRange } from '../utils/random-range';
 import { capitalize } from '../utils/capitalize';
+import { parseRange } from '../utils/parse-range';
 
 @Injectable()
 export class LoremService {
@@ -16,11 +24,19 @@ export class LoremService {
     return res;
   }
 
-  generateLoremSentences(amount: number): string[] {
+  generateLoremSentences(
+    amount: number,
+    wordsAmountRange?: number | string,
+  ): string[] {
     const res = [];
 
+    let [min, max] = parseRange(wordsAmountRange);
+
+    min ||= DEFAULT_MIN_WORDS;
+    max ||= DEFAULT_MAX_WORDS;
+
     for (let i = 0; i < amount; i++) {
-      const sentence = this.generateLoremWords(randomRange(5, 15)).join(' ');
+      const sentence = this.generateLoremWords(randomRange(min, max)).join(' ');
 
       res.push(`${capitalize(sentence)}.`);
     }
@@ -28,18 +44,36 @@ export class LoremService {
     return res;
   }
 
-  generateLoremParagraphs(amount: number): string[] {
+  generateLoremParagraphs(
+    amount: number,
+    wordsAmountRange?: number | string,
+    sentenceAmountRange?: number | string,
+  ): string[] {
     const paragraphs = [];
+
+    let [minSentenceAmount, maxSentenceAmount] =
+      parseRange(sentenceAmountRange);
+
+    minSentenceAmount ||= DEFAULT_MIN_SENTENCES;
+    maxSentenceAmount ||= DEFAULT_MAX_SENTENCES;
 
     const firstParagraph = [
       LOREM_FIRST,
-      ...this.generateLoremSentences(randomRange(3, 7)),
+      ...this.generateLoremSentences(
+        randomRange(minSentenceAmount - 1, maxSentenceAmount - 1),
+        wordsAmountRange,
+      ),
     ].join(' ');
 
     paragraphs.push(firstParagraph);
 
     for (let i = 0; i < amount - 1; i++) {
-      paragraphs.push(this.generateLoremSentences(randomRange(4, 8)).join(' '));
+      paragraphs.push(
+        this.generateLoremSentences(
+          randomRange(minSentenceAmount, maxSentenceAmount),
+          wordsAmountRange,
+        ).join(' '),
+      );
     }
 
     return paragraphs;
