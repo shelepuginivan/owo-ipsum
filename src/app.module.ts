26667c356b52b130fbacb59c7aa661ee56222ad0
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-19 19:04:07 +0300

    Added `ServeStaticModule` for sending static html/css and js (client)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ApiModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ApiModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
 })
 export class AppModule {}
