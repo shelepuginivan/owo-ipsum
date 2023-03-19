263b33137fe50b8550f9ac76e363a9dd17428aaa
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-19 13:06:46 +0300

    Added doc string

--- src/utils/random-range.ts
+++ src/utils/random-range.ts
@@ -1,2 +1,8 @@
-export const randomRange = (from: number, to: number) =>
+/*
+ * Returns a random number from the half-closed interval `[from; to)`
+ * @param {number} from - lower limit of the interval (included)
+ * @param {number} to - upper limit of the interval (excluded)
+ * @return random number from the interval
+ * */
+export const randomRange = (from: number, to: number): number =>
   Math.floor(Math.random() * (to - from)) + from;
