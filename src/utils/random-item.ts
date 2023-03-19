537247420d02ec9805bb28444e521f03473168d6
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-19 13:01:01 +0300

    Added doc string

--- src/utils/random-item.ts
+++ src/utils/random-item.ts
@@ -1,2 +1,7 @@
-export const randomItem = <T>(array: T[]) =>
+/*
+ * Returns a random array element
+ * @param {T[]} array - array from which an element is returned
+ * @returns {T} random element of the given array
+ * */
+export const randomItem = <T>(array: T[]): T =>
   array[Math.floor(Math.random() * array.length)];
