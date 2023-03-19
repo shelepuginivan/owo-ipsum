8bc0ea13e19a3d920efae7324d1139b16dd920f8
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-19 21:18:16 +0300

    Added home page script

---
+++ client/index.js
@@ -0,0 +1,15 @@
+const generatorForm = document.getElementById('generator-form');
+const sectionResult = document.getElementById('section-result');
+
+generatorForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const { number, stutter, face, action } = generatorForm;
+
+  const url = `/api/paragraphs?number=${number.value}&stutter=${stutter.value}&face=${face.value}&action=${action.value}&format=html`;
+  const res = await fetch(url);
+  const html = await res.text();
+
+  sectionResult.innerHTML = '<h1>Result:</h1>' + html;
+});
