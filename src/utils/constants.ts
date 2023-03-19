66524ab322ef918a96fb401b458894b1dc84448a
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-19 12:49:31 +0300

    Decreased default maximum of words in sentence (15 -> 10)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const DEFAULT_ACTION_PROBABILITY = 0.05;
 export const DEFAULT_FACE_PROBABILITY = 0.05;
 export const DEFAULT_STUTTER_PROBABILITY = 0.1;
 export const DEFAULT_MIN_WORDS = 5;
-export const DEFAULT_MAX_WORDS = 15;
+export const DEFAULT_MAX_WORDS = 10;
 export const DEFAULT_MIN_SENTENCES = 3;
 export const DEFAULT_MAX_SENTENCES = 8;
 
