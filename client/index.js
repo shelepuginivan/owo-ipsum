147f555a93166413c8aa7bba9198698a66fe8e86
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-23 01:34:22 +0300

    Added start with lorem option checkbox in generator

--- client/index.js
+++ client/index.js
@@ -5,9 +5,9 @@ generatorForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   e.stopPropagation();
 
-  const { number, stutter, face, action } = generatorForm;
+  const { number, stutter, face, action, lorem } = generatorForm;
 
-  const url = `/api/paragraphs?number=${number.value}&stutter=${stutter.value}&face=${face.value}&action=${action.value}&format=html`;
+  const url = `/api/paragraphs?number=${number.value}&stutter=${stutter.value}&face=${face.value}&action=${action.value}&lorem=${lorem.checked}&format=html`;
   const res = await fetch(url);
   const html = await res.text();
 
